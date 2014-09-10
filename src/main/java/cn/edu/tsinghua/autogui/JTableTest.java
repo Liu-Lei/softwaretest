a540f4b3b018c6f932705c126a996b2d83c195d4
liulei <lo2007@126.com>
2014-09-10 16:19:31 +0800

    Remove the FontChooser

---
+++ src/main/java/cn/edu/tsinghua/autogui/JTableTest.java
@@ -0,0 +1,16 @@
+package cn.edu.tsinghua.autogui;
+
+import javax.swing.*;
+
+/**
+ * Created by liulei on 2014/9/1.
+ */
+public class JTableTest {
+    private JPanel panel1;
+    private JList list1;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
